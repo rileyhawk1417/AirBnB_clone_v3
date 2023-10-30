c157d7df0fdd9c5fb05e7b365f9b04eb4c8efb24
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-10-30 16:41:37 +0200

    chore: comment out non-existant files

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,6 @@
 """ Init defintion for views package"""
 from flask import Blueprint
 
-
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
@@ -11,5 +10,7 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
+
+# TODO: Add these files
+# from api.v1.views.places_reviews import *
+# from api.v1.views.places_amenities import *
