e1aa50a0bab4efcf61f5195b866a46783fce55b4
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-10-31 20:16:15 +0200

    doc: fix according to pycodestyle

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,10 +6,13 @@ from models import storage
 from models.city import City
 from models.state import State
 
+# Base urls
+c_url = "/states/<string:state_id>/cities"
+# city id url
+idurl = "/cities/<string:city_id>"
 
-@app_views.route(
-    "/states/<string:state_id>/cities", methods=["GET"], strict_slashes=False
-)
+
+@app_views.route("{}".format(c_url), methods=["GET"], strict_slashes=False)
 def get_cities(state_id):
     """Retrieves the list of all City objects of a State.
 
@@ -31,11 +34,9 @@ def get_cities(state_id):
     # sc: state_cities
     res = jsonify(cities_)
     return res
-    # cities = [city.to_dict() for city in state_obj.cities]
-    # return jsonify(cities)
 
 
-@app_views.route("/cities/<string:city_id>", methods=["GET"], strict_slashes=False)
+@app_views.route("{}".format(cid_url), methods=["GET"], strict_slashes=False)
 def get_city(city_id):
     """Retrieves a City object based on `city_id`.
 
@@ -52,7 +53,7 @@ def get_city(city_id):
     return jsonify(city_obj.to_dict())
 
 
-@app_views.route("/cities/<string:city_id>", methods=["DELETE"], strict_slashes=False)
+@app_views.route("{}".format(idurl), methods=["DELETE"], strict_slashes=False)
 def delete_city(city_id):
     """Deletes a City object based on `city_id`.
 
@@ -71,9 +72,7 @@ def delete_city(city_id):
     return jsonify({})
 
 
-@app_views.route(
-    "/states/<string:state_id>/cities", methods=["POST"], strict_slashes=False
-)
+@app_views.route("{}".format(c_url), methods=["POST"], strict_slashes=False)
 def add_city(state_id):
     """Creates a City object using `state_id` and HTTP body request fields.
 
@@ -99,7 +98,7 @@ def add_city(state_id):
     return jsonify(new_city.to_dict()), 201
 
 
-@app_views.route("/cities/<string:city_id>", methods=["PUT"], strict_slashes=False)
+@app_views.route("{}".format(idurl), methods=["PUT"], strict_slashes=False)
 def edit_city(city_id):
     """Edit a City object using `city_id` and HTTP body request fields.
 
