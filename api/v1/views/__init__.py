7d4ff5e63562211893a778cc93a73bd7b889ff2c
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-10-31 18:00:55 +0200

    fix: ignore pystyle & restore original file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ Init defintion for views package"""
 from flask import Blueprint
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -8,7 +11,6 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 # TODO: Add these files
 # from api.v1.views.places_reviews import *
 # from api.v1.views.places_amenities import *
