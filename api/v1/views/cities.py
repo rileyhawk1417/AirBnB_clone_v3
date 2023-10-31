6c8a1cbfdfca808817f3073e11117d5d4182b5b1
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-10-31 22:02:13 +0200

    fix: move up request.get_json()

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -87,15 +87,15 @@ def add_city(state_id):
         Raise 404 error if `state_id` is not linked to any State object.
     """
     state_obj = storage.get(State, state_id)
+    req_json = request.get_json()
     if state_obj is None:
         abort(404)
-    if request.json is None:
+    if req_json is None:
         return "Not a JSON", 400
-    fields = request.get_json()
-    if fields.get("name") is None:
+    if req_json.get("name") is None:
         return "Missing name", 400
-    fields["state_id"] = state_id
-    new_city = City(**fields)
+    req_json["state_id"] = state_id
+    new_city = City(**req_json)
     new_city.save()
     return jsonify(new_city.to_dict()), 201
 
