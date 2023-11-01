a7db641044dc63670f135ec3d4cbcbc89af8f63c
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-11-01 18:40:44 +0200

    chore: update city class along with state model & city model

--- models/city.py
+++ models/city.py
@@ -2,19 +2,19 @@
 """ holds class City"""
 import models
 from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """Representation of city """
+    """Representation of city"""
+
     if models.storage_t == "db":
-        __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        __tablename__ = "cities"
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
+        orph = "all delete-orphan"
+        places = relationship("Place", backref="cities", cascade=orph)
     else:
         state_id = ""
         name = ""
--- models/state.py
+++ models/state.py
@@ -3,18 +3,18 @@
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """Representation of state """
+    """Representation of state"""
+
     if models.storage_t == "db":
-        __tablename__ = 'states'
+        __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        orph = "all delete-orphan"
+        cities = relationship("City", backref="state", cascade=orph)
     else:
         name = ""
 
@@ -23,6 +23,7 @@ class State(BaseModel, Base):
         super().__init__(*args, **kwargs)
 
     if models.storage_t != "db":
+
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
